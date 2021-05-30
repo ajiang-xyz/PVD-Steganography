29b00d447a3b1e6a2ae53420922e7187cdaf8675
Alex Jiang <50186316+ajiang-xyz@users.noreply.github.com>
2021-05-30 13:35:34 -0700

    Update compact code, bug fix in line counter

--- PVD/line counter.py
+++ PVD/line counter.py
@@ -2,5 +2,6 @@ import sys
 with open(sys.argv[1],'r') as D:
     B=0;C=0
     for A in D:
-        if A.strip()!=''and'#'not in A.strip():B,C=B+1,C+1
+        if A.strip()!=''and'#'not in A.strip():B+=1
+        C+=1
     print(f'{B} out of {C} lines in "{sys.argv[1]}" are code')
